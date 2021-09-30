d3b2988431f20f652aef141308f735807cc723c6
gpaetzold <76201189+gpaetzold@users.noreply.github.com>
2021-09-30 16:04:51 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,16 @@
+let lightness = 0
+basic.showIcon(IconNames.Happy)
 basic.forever(function () {
-	
+    lightness = input.lightLevel()
+    if (lightness > 10) {
+        basic.clearScreen()
+    } else {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    }
 })
