608c6a21000c111b97b4e3ff1c7e2885eed3a8a8
gpaetzold <76201189+gpaetzold@users.noreply.github.com>
2021-10-01 12:03:54 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,8 +3,10 @@ basic.showIcon(IconNames.Happy)
 basic.forever(function () {
     lightness = input.lightLevel()
     if (lightness > 10) {
+        basic.showIcon(IconNames.Square)
         pins.digitalWritePin(DigitalPin.P1, 0)
     } else {
         pins.digitalWritePin(DigitalPin.P1, 1)
+        basic.showIcon(IconNames.SmallSquare)
     }
 })
